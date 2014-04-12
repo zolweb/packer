e066bdd622a8df77180278302a482d1ac20cfbfe
Jeremy Perret <jeremy@devster.org>
2014-04-12 18:09:20 +0200

    add workaround for guest additions 4.3.10

--- scripts/vbox_guest_additions.sh
+++ scripts/vbox_guest_additions.sh
@@ -6,3 +6,9 @@ sudo mount -o loop VBoxGuestAdditions.iso /mnt/disk
 cd /mnt/disk
 sudo sh VBoxLinuxAdditions.run install --force || :
 sudo rm -f ~/VBoxGuestAdditions.iso
+
+# Workaround to fix the Guest additions bug in 4.3.10
+# https://www.virtualbox.org/ticket/12879
+if [ `cat ~/.vbox_version` = "4.3.10" ]; then
+    sudo ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
+fi
